822e7ce925c2b87998502c1b5fb389a45e26df1f
yenanw <yenanw@proton.me>
2024-11-29 17:19:31 +0100

    Add CIFAR100 support

---
+++ main_cifar100.py
@@ -0,0 +1,403 @@
+import torch
+import torch.nn.functional as F
+import torchvision
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, Subset, ConcatDataset
+from torchvision.datasets import CIFAR100
+import torchvision.transforms as transforms
+
+import random, os, sys, re
+import numpy as np
+from math import sqrt
+from matplotlib import pyplot as plt
+import pandas as pd
+import copy
+
+import time
+import tracemalloc
+import psutil
+from torchvision.transforms.transforms import ToTensor
+
+from fedlab.utils.dataset import CIFAR100Partitioner
+from fedlab.utils.functional import partition_report, save_dict
+
+from args_cifar10_c2 import args_parser
+import server_se1 as server
+import model
+
+from utils.global_test import (
+    test_on_globaldataset,
+    globalmodel_test_on_localdataset,
+    globalmodel_test_on_specifdataset,
+)
+from utils.local_test import test_on_localdataset
+from utils.training_loss import train_loss_show, train_localacc_show
+from utils.sampling import testset_sampling, trainset_sampling, trainset_sampling_label
+from utils.tSNE import FeatureVisualize
+from synthetic_data import IndexedDataset, SyntheticCIFAR10
+
+args = args_parser()
+
+
+class Filter(object):
+    def __init__(self, stream, re_pattern):
+        self.stream = stream
+        self.pattern = (
+            re.compile(re_pattern) if isinstance(re_pattern, str) else re_pattern
+        )
+        self.triggered = False
+
+    def __getattr__(self, attr_name):
+        return getattr(self.stream, attr_name)
+
+    def write(self, data):
+        if data == "\n" and self.triggered:
+            self.triggered = False
+        else:
+            if self.pattern.search(data) is None:
+                self.stream.write(data)
+                self.stream.flush()
+            else:
+                # caught bad pattern
+                self.triggered = True
+
+    def flush(self):
+        self.stream.flush()
+
+
+# example
+sys.stdout = Filter(
+    sys.stdout, r"WARNING: The input does not fit in a single ciphertext"
+)  # filter out any line which contains "Read -1" in it
+
+
+def seed_torch(seed=args.seed):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def get_mean_std(loader):
+    # Compute the mean and standard deviation of the images for each channel
+    # in the dataset
+    mean = torch.zeros(3)
+    std = torch.zeros(3)
+
+    for images, _ in loader:
+        mean += images.mean(axis=(0, 2, 3))
+        std += images.std(axis=(0, 2, 3))
+
+    mean /= len(loader)
+    std /= len(loader)
+
+    return mean, std
+
+
+def run_FedFA():
+    seed_torch()
+
+    results_path = f"results/{args.dataset.lower()}/fhe{str(args.ratio).replace('.', '_')}_inter{args.AR}_{args.SR}/"
+    args.path = results_path
+    if not os.path.exists(results_path):
+        print(f"Creating directory {results_path}")
+        os.makedirs(results_path)
+    else:
+        print(f"Directory {results_path} already exists, proceeding...")
+
+    similarity = False
+    save_models = True
+    Train_model = True
+    C = "2CNN_2"
+
+    root = "data/CIFAR100/"
+
+    trainset_unnorm = CIFAR100(
+        root=root, train=True, download=True, transform=ToTensor()
+    )
+    train_loader = DataLoader(trainset_unnorm, batch_size=64)
+    means, stds = get_mean_std(train_loader)
+    print(f"mean: {means.numpy()}\nstd: {stds.numpy()}")
+    del trainset_unnorm
+    del train_loader
+
+    specf_model = model.ClientModel(args, name="cifar100").to(args.device)
+    trans_cifar100 = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=means, std=stds),
+        ]
+    )
+    trainset = CIFAR100(root=root, train=True, download=True, transform=trans_cifar100)
+    testset = CIFAR100(root=root, train=False, download=True, transform=trans_cifar100)
+
+    auth_idxs = []
+    synth_idxs = []
+    for cls in range(len(trainset.classes)):
+        cls_idx = np.where(np.array(trainset.targets) == cls)[0]
+        cls_len = len(cls_idx)
+        np.random.shuffle(cls_idx)
+
+        auth_idx = cls_idx[: cls_len // 2]
+        synth_idx = cls_idx[cls_len // 2 :]
+
+        auth_idxs.extend(auth_idx)
+        synth_idxs.extend(synth_idx)
+
+    auth_dst = IndexedDataset(trainset, auth_idxs)
+    synth_dst = IndexedDataset(trainset, synth_idxs)
+
+    num_classes = args.num_classes
+    num_clients = args.K
+    number_perclass = args.num_perclass
+
+    col_names = [f"class{i}" for i in range(num_classes)]
+    print(col_names)
+    # hist_color = "#4169E1"
+    # plt.rcParams["figure.facecolor"] = "white"
+
+    # perform partition
+    noniid_labeldir_part = CIFAR100Partitioner(
+        auth_dst.targets,
+        num_clients=num_clients,
+        balance=None,
+        partition="shards",
+        num_shards=200,
+        seed=1,
+    )
+    # generate partition report
+    csv_file = (
+        "data/CIFAR100/cifar100_noniid_labeldir_clients_"
+        + f"enc{int(args.ratio * 100)}_inter{args.AR}_{args.SR}.csv"
+    )
+    partition_report(
+        auth_dst.targets,
+        noniid_labeldir_part.client_dict,
+        class_num=num_classes,
+        verbose=False,
+        file=csv_file,
+    )
+
+    noniid_labeldir_part_df = pd.read_csv(csv_file, header=1)
+    noniid_labeldir_part_df = noniid_labeldir_part_df.set_index("client")
+    for col in col_names:
+        noniid_labeldir_part_df[col] = (
+            noniid_labeldir_part_df[col] * noniid_labeldir_part_df["Amount"]
+        ).astype(int)
+
+    # select first 10 clients for bar plot
+    # noniid_labeldir_part_df[col_names].iloc[:10].plot.barh(stacked=True)
+    # # plt.tight_layout()
+    # plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    # plt.xlabel("sample num")
+    # plt.savefig(
+    #     f"data/CIFAR10//cifar10_noniid_labeldir_clients_10.png",
+    #     dpi=400,
+    #     bbox_inches="tight",
+    # )
+
+    # split dataset into training and testing
+
+    trainset_sample_rate = args.trainset_sample_rate
+
+    rare_class_nums = 0
+    dict_users_train = trainset_sampling_label(
+        args, auth_dst, trainset_sample_rate, rare_class_nums, noniid_labeldir_part
+    )
+    dict_users_test = testset_sampling(
+        args, testset, number_perclass, noniid_labeldir_part_df
+    )
+
+    # training_number = {j: {} for j in range(args.K)}
+    #
+    # for i in range(args.K):
+    #     training_number[i] = {j: 0 for j in range(num_classes)}
+    #     label_class = set(
+    #         np.array(trainset.targets)[list(dict_users_train[i])].tolist()
+    #     )
+    #     # print(list(label_class))
+    #     for k in label_class:
+    #         training_number[i][k] = list(
+    #             np.array(trainset.targets)[list(dict_users_train[i])]
+    #         ).count(k)
+    #
+    # df_training_number = []
+    # df_training_number = pd.DataFrame(df_training_number)
+    # for i in range(args.K):
+    #     temp = pd.Series(training_number[i])
+    #     df_training_number[i] = temp
+    #
+    # df_training_number["Col_sum"] = df_training_number.apply(lambda x: x.sum(), axis=1)
+    # df_training_number.loc["Row_sum"] = df_training_number.apply(lambda x: x.sum())
+    #
+    # test_number = {j: {} for j in range(args.K)}
+    #
+    # for i in range(args.K):
+    #     test_number[i] = {j: 0 for j in range(num_classes)}
+    #     label_class = set(np.array(testset.targets)[list(dict_users_test[i])].tolist())
+    #     # print(list(label_class))
+    #     for k in label_class:
+    #         test_number[i][k] = list(
+    #             np.array(testset.targets)[list(dict_users_test[i])]
+    #         ).count(k)
+    #
+    # df_test_number = []
+    # df_test_number = pd.DataFrame(df_test_number)
+    # for i in range(args.K):
+    #     temp = pd.Series(test_number[i])
+    #     df_test_number[i] = temp
+    #
+    # df_test_number["Col_sum"] = df_test_number.apply(lambda x: x.sum(), axis=1)
+    # df_test_number.loc["Row_sum"] = df_test_number.apply(lambda x: x.sum())
+    #
+    # # perform partition
+    # iid_part = FMNISTPartitioner(
+    #     trainset.targets, num_clients=num_clients, partition="iid", seed=1
+    # )
+    # # generate partition report
+    # csv_file = "data/CIFAR10/cifar10_iid_clients_10.csv"
+    # partition_report(
+    #     trainset.targets,
+    #     iid_part.client_dict,
+    #     class_num=num_classes,
+    #     verbose=False,
+    #     file=csv_file,
+    # )
+    #
+    # iid_part_df = pd.read_csv(csv_file, header=1)
+    # iid_part_df = iid_part_df.set_index("client")
+    # for col in col_names:
+    #     iid_part_df[col] = (iid_part_df[col] * iid_part_df["Amount"]).astype(int)
+    #
+    # # select first 10 clients for bar plot
+    # iid_part_df[col_names].iloc[:10].plot.barh(stacked=True)
+    # # plt.tight_layout()
+    # plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    # plt.xlabel("sample num")
+    # plt.savefig(
+    #     f"data/CIFAR10/cifar10_iid_clients_10.png", dpi=400, bbox_inches="tight"
+    # )
+    # dict_users_train_iid = trainset_sampling_label(
+    #     args, trainset, trainset_sample_rate, rare_class_nums, iid_part
+    # )
+    # dict_users_test_iid = testset_sampling(args, testset, number_perclass, iid_part_df)
+    #
+    # training_number = {j: {} for j in range(args.K)}
+    #
+    # for i in range(args.K):
+    #     training_number[i] = {j: 0 for j in range(num_classes)}
+    #     label_class = set(
+    #         np.array(trainset.targets)[list(dict_users_train_iid[i])].tolist()
+    #     )
+    #     # print(list(label_class))
+    #     for k in label_class:
+    #         training_number[i][k] = list(
+    #             np.array(trainset.targets)[list(dict_users_train_iid[i])]
+    #         ).count(k)
+    #
+    # df_training_number = []
+    # df_training_number = pd.DataFrame(df_training_number)
+    # for i in range(args.K):
+    #     temp = pd.Series(training_number[i])
+    #     df_training_number[i] = temp
+    #
+    # df_training_number["Col_sum"] = df_training_number.apply(lambda x: x.sum(), axis=1)
+    # df_training_number.loc["Row_sum"] = df_training_number.apply(lambda x: x.sum())
+
+    print("model:", args.model)
+    total_params = sum(p.numel() for p in specf_model.parameters())
+    print("parameters:", total_params)
+
+    syn_noniid_labeldir_part = CIFAR100Partitioner(
+        synth_dst.targets,
+        num_clients=num_clients,
+        balance=None,
+        partition="shards",
+        num_shards=200,
+        seed=1,
+    )
+    syn_dict_users = trainset_sampling_label(
+        args,
+        synth_dst,
+        trainset_sample_rate,
+        rare_class_nums,
+        syn_noniid_labeldir_part,
+    )
+    serverz = server.Server(
+        # args, specf_model, trainset, dict_users_train
+        args,
+        specf_model,
+        auth_dst,
+        dict_users_train,
+        synth_dst,
+        syn_dict_users,
+    )  # dict_users指的是user的local dataset索引
+    print("global_model:", serverz.nn.state_dict)
+
+    server_feature = serverz
+
+    if Train_model:
+        (
+            global_modelfa,
+            similarity_dictfa,
+            client_modelsfa,
+            loss_dictfa,
+            clients_indexfa,
+            acc_listfa,
+        ) = server_feature.fedfa_anchorloss(
+            testset,
+            dict_users_test[0],
+            similarity=similarity,
+            test_global_model_accuracy=True,
+        )
+    else:
+        if similarity:
+            similarity_dictfa = torch.load(
+                "results/Test/label skew/cifar100/fedfa/seed{}/similarity_dictfa_{}E_{}class.pt".format(
+                    args.seed, args.E, C
+                )
+            )
+        acc_listfa = torch.load(
+            "results/Test/label skew/cifar100/fedfa/seed{}/acc_listfa_{}E_{}class.pt".format(
+                args.seed, args.E, C
+            )
+        )
+        global_modelfa = server_feature.nn
+        client_modelsfa = server_feature.nns
+        path_fedfa = "results/Test/label skew/cifar100/fedfa/seed{}/global_model_fedfa_{}E_{}class".format(
+            args.seed, args.E, C
+        )
+        global_modelfa.load_state_dict(torch.load(path_fedfa))
+        for i in range(args.K):
+            path_fedfa = "results/Test/label skew/cifar100/fedfa/seed{}/client{}_model_fedfa_{}E_{}class".format(
+                args.seed, i, args.E, C
+            )
+            client_modelsfa[i] = copy.deepcopy(global_modelfa)
+            client_modelsfa[i].load_state_dict(torch.load(path_fedfa))
+
+    # if save_models:
+    #     result_checkpoint_path = results_path + "cifar10_plain_model.ckpt"
+    #     torch.save(
+    #         {
+    #             "model": global_modelfa.state_dict(),
+    #             "acc_list": acc_listfa,
+    #             "loss_dict": loss_dictfa,
+    #         },
+    #         result_checkpoint_path,
+    #     )
+    #     print("Finished. Saving the model and ML metrics...")
+
+
+if __name__ == "__main__":
+    print(f"setting: {int(args.C * args.K)}/{args.K} active clients")
+
+    start_time = time.time()
+    tracemalloc.start()
+    psutil.cpu_percent()
+    run_FedFA()
+    end_time = time.time()
+    print("Execution Time: ", end_time - start_time)
--- model.py
+++ model.py
@@ -6,19 +6,23 @@ class ClientModel(nn.Module):
     def __init__(self, args, name):
         super().__init__()
         self.args = args
-        self.achitecture = args.model
         self.name = name
 
-        match self.achitecture.lower():
-            case "cnn":
-                self.n_cls = 10
-                self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5)
-                self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5)
-                self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-                self.fc1 = nn.Linear(64 * 5 * 5, 384)
-                self.fc2 = nn.Linear(384, args.dims_feature)  # args.dims_feature=192
-                self.classifier = nn.Linear(args.dims_feature, self.n_cls)
-            case "lenet5":
+        match self.name.lower():
+            case "cifar100":
+                self.n_cls = 100
+                self.conv1 = nn.Conv2d(
+                    in_channels=3, out_channels=6, kernel_size=5, padding=2
+                )
+                self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+                self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+                self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+                self.fc1 = nn.Linear(576, 240)
+                self.fc2 = nn.Linear(
+                    240, self.args.dims_feature
+                )  # args.dims_feature = 168
+                self.classifier = nn.Linear(self.args.dims_feature, self.n_cls)
+            case "cifar10":
                 self.n_cls = 10
                 self.conv1 = nn.Conv2d(
                     in_channels=3, out_channels=6, kernel_size=5, padding=2
@@ -32,18 +36,20 @@ class ClientModel(nn.Module):
                 )  # args.dims_feature = 84
                 self.classifier = nn.Linear(self.args.dims_feature, self.n_cls)
             case _:
-                raise ValueError(f"{self.name!r} is not a supported architecture")
+                raise ValueError(f"{self.name!r} is not a supported dataset")
 
     def forward(self, x):
-        match self.achitecture.lower():
-            case "cnn":
-                x = self.pool(F.sigmoid(self.conv1(x)))
-                x = self.pool(F.sigmoid(self.conv2(x)))
-                x = x.view(-1, 64 * 5 * 5)
+        match self.name.lower():
+            case "cifar100":
+                x = F.sigmoid(self.conv1(x))
+                x = self.pool1(x)
+                x = F.sigmoid(self.conv2(x))
+                x = self.pool2(x)
+                x = x.view(x.shape[0], -1)
                 x = F.sigmoid(self.fc1(x))
                 y_feature = F.sigmoid(self.fc2(x))
                 x = self.classifier(y_feature)
-            case "lenet5":
+            case "cifar10":
                 x = F.sigmoid(self.conv1(x))
                 x = self.pool1(x)
                 x = F.sigmoid(self.conv2(x))
@@ -53,6 +59,6 @@ class ClientModel(nn.Module):
                 y_feature = F.sigmoid(self.fc2(x))
                 x = self.classifier(y_feature)
             case _:
-                raise ValueError(f"{self.name!r} is not a supported architecture")
+                raise ValueError(f"{self.name!r} is not a supported dataset")
 
         return y_feature, x
